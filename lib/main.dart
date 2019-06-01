206dc1ec2378a740c6da106090afd6003c214d8b
hugoortins <51232118+hugoortins@users.noreply.github.com>
2019-06-01 14:59:27 +0000

    Initial-Search

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,36 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Stack Overflow',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Stack Overflow Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +29,111 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //var searchResults;
+  //int radioValue;
+  @override
+  initState() {
+    super.initState();
+    this.getSearch(null);
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<List<Question>> getSearch(String search) async {
+    List<Question> list;
+
+    String url =
+        "https://api.stackexchange.com/2.2/search/advanced?pagesize=10&order=desc&sort=activity&tagged=flutter&site=stackoverflow";
+    final response = await http.get(url);
+
+    if (response.statusCode == 200 && response.body != null) {
+      final jsonResult = json.decode(utf8.decode(response.bodyBytes));
+      //var data = json.decode(response.body);
+      var rest = jsonResult["items"] as List;
+      list = rest.map<Question>((json) => Question.fromJson(json)).toList();
+      return list;
+    } else {
+      return null;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          Text(""),
+          FutureBuilder<List<Question>>(
+              future: getSearch(
+                  null), // a previously-obtained Future<String> or null
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<Question>> snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                    return Text('Press button to start.');
+                  case ConnectionState.active:
+                  case ConnectionState.waiting:
+                    return Text("Please wait.");
+                  case ConnectionState.done:
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return ListView(
+                        shrinkWrap: true,
+                        children: snapshot.data.map(getQuestionItem).toList(),
+                      );
+                    }
+                }
+                return null; // unreachable
+              })
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget getQuestionItem(Question question) {
+    return ListTile(
+      title: Text(question.title),
+      trailing: RaisedButton(
+          child: Icon(Icons.keyboard_arrow_right),
+          onPressed: () {
+            _launchUrl('https://stackoverflow.com/questions/' +
+                question.question_id.toString());
+          }),
+      /*onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ChatScreen(question_id: question.question_id),
+          ),
+        );
+                  },*/
+    );
+  }
+
+  _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Não foi possível abrir: $url';
+    }
+  }
+}
+
+class Question {
+  final int question_id;
+  final String title;
+
+  Question({
+    this.question_id,
+    this.title,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      question_id: json['question_id'],
+      title: json['title'],
     );
   }
 }
